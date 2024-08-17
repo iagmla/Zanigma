a90fbe16a7d27ef4f060ed4f8c94cba5cd75e65b
Karl Zander <karl.c.zander@gmail.com>
2024-08-16 19:01:02 -0700

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# Zanigma
+
+Zanigma is an effort to repair the German Enigma machine. At the moment, the issue of a letter not being able to encrypt to itself has been fixed. Zanigma has much more uniform properties than the original Enigma machine, however, it does not classify as an ideal stream cipher.
+
+What fixed the issue? A new wired, stepping reflector. Prior to a letter passing to the reflector, the reflector is stepped.
---
+++ z.py
@@ -0,0 +1,15 @@
+from zanigma import ZANIGMA
+
+rotor1 = ["EKMFLGDQVZNTOWYHXUSPAIBRCJ","Q"]
+rotor2 = ["AJDKSIRUXBLHWTMCQGZNPYFVOE", "E"]  
+rotor3 = ["BDFHJLCPRTXVZNYEIWGAKMUSQO", "V"]  
+reflector = "YXWVUTSRQPONMLKJIHGFEDCBAZ"
+plugs = ['AC', 'BE']
+settings = "AAAB"
+msg = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+e = ZANIGMA(rotor3, rotor2, rotor1, reflector)
+ctxt = e.input(msg, settings)
+print(ctxt)
+d = ZANIGMA(rotor3, rotor2, rotor1, reflector)
+ptxt = d.input(ctxt, settings)
+print(ptxt)
